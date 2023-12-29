530fae4aa8b27995b715ba7560815c2696eadaf9
SonPhatTranDeveloper <phat.tran.developer@gmail.com>
2023-12-29 14:09:32 +1100

    Add Jupyter Notebook and data

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -7,7 +7,9 @@ import matplotlib.pyplot as plt
 
 
 # Generate some random points X uniformly
-X = np.random.uniform(0, 10, 30)
+N = 5
+X = np.random.uniform(0, 10, N)
+X = np.sort(X)
 
 # True linear function
 m0 = 1
@@ -15,7 +17,7 @@ m1 = 2
 
 # Generate label
 beta = 0.5
-t = m0 + m1 * X + np.random.normal(loc=0, scale=1/beta, size=30)
+t = m0 + m1 * X + np.random.normal(loc=0, scale=1/beta, size=N)
 
 
 if __name__ == "__main__":
